7fb3c63ff4c7312fdad7c3ddfbd8329f555e5a7c
shado18 <wilterplay18@gmail.com>
2023-11-05 13:03:03 -0400

    pruebas tablas

--- src/tablas.sql
+++ src/tablas.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 04-11-2023 a las 22:20:27
--- Versión del servidor: 8.0.31
--- Versión de PHP: 7.4.33
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
